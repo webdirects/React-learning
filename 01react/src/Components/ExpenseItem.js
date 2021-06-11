397d9dd68a38c4ef60850285a0071d64b587fd15
DESKTOP-GI8EU5B\progr <valomanas99@gmail.com>
2021-06-11 17:20:30 +0100

    Learning Components

---
+++ 01react/src/Components/ExpenseItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ExpenseItem = () => {
+  return (
+    <>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+      </div>
+      <div>
+        <h2>$294.67</h2>
+      </div>
+    </>
+  );
+};
+
+export default ExpenseItem;
