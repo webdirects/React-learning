7ff4324b2165a0705536df959125dc30cb59f664
DESKTOP-GI8EU5B\progr <valomanas99@gmail.com>
2021-06-12 11:30:56 +0100

    minize the app.js

--- 01react/src/App.js
+++ 01react/src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./Components/ExpenseItem";
+import Expenses from "./Components/Expenses";
 
 const App = () => {
   const expenses = [
@@ -26,21 +26,7 @@ const App = () => {
   return (
     <div>
       <h1>Hello World</h1>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
+      <Expenses items={expenses} />
     </div>
   );
 };
