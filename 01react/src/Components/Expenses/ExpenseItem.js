41e5ba5b8e24eaac209467823da0a9d84c40e251
DESKTOP-GI8EU5B\progr <valomanas99@gmail.com>
2021-06-13 00:11:50 +0100

    added states

--- 01react/src/Components/Expenses/ExpenseItem.js
+++ 01react/src/Components/Expenses/ExpenseItem.js
@@ -1,21 +1,26 @@
-import React from "react";
-import "./ExpenseItem.css";
+import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  
+  const [title, setTile] = useState(props.title);
+
+  const clickHandler = () => {
+    setTile('Updated');
+  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className="expense-item__price">
         <h2>${props.amount}</h2>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
